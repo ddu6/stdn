6a832d7434340962df1d6407207dc4910be14bb7
DDU <ddu6@protonmail.com>
2021-09-02 21:54:02 +0800

    update

--- test/main.js
+++ test/main.js
@@ -2,16 +2,14 @@ const stdn=require('../dist/mod')
 const example=`{'a_1=1'}
 {display,'a_2=2'}
 ['Let '{'a_3=3'}.]
-{label eq1,equation[{'a_4=4'}]}
-{label eq2,
-    // avoid to be a dog in the manger
-    orbit alone,mark{[[{label eq1,class plain,ref[]}{'\\''}]]},equation[{'a_5=5'}]}
+{id eq1,equation[{'a_4=4'}]}
+{id eq2,mark{[[{id eq1,class plain,ref[]}{'\\''}]]},equation[{'a_5=5'}]}
 {class display,aligned[
-    ['a_6&=6\\ \\text{see '{label eq2,ref[]}'}\\\\\\\\']
+    ['a_6&=6\\ \\text{see '{id eq2,ref[]}'}\\\\\\\\']
     'a_{1000}&=1000'
 ]}
-{label th1,mark A,desc DDU,theorem[STDN is easy to use.]}
+{id th1,mark A,desc DDU,theorem[STDN is easy to use.]}
 {proof[We leave it to the reader.]}
-{label co1,corollary[{label eq3,orbit alone,mark{'\\square'},class plain,equation[STDN is a good language.]}]}`
+{id co1,corollary[{id eq3,mark{'\\square'},class plain,equation[STDN is a good language.]}]}`
 console.log(stdn.stringify(stdn.parse(example)))
 console.log(stdn.format(example))
