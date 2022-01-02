27d47d59aec0159dac798f6a2d87155d01060e48
DDU <ddu6@protonmail.com>
2022-01-03 02:54:17 +0800

    update

--- test/main.js
+++ test/main.js
@@ -1,14 +1,14 @@
-const stdn=require('../commonjs/dist/mod')
-let example=`{id t1, mark FLT, desc Fermat, theorem [
+const stdn = require('../commonjs/dist/mod')
+let example = `{id t1, mark FLT, desc Fermat, theorem [
     ['Let '{'n'}' be an integer greater than '{'2'}', then there are no positive integers '{'a,b,c'}' satisfying']
     {display, 'a^n+b^n=c^n.'}
 ]}
 ['The first successful proof of '{ref-id t1, ref []}' was released in 1994 by Andrew Wiles.']`
-console.log(stdn.format(example)===example)
-example=`{id t1, mark FLT, desc Fermat, theorem [
+console.log(stdn.format(example) === example)
+example = `{id t1, mark FLT, desc Fermat, theorem [
     ['Let '{'n'}' be an integer greater than '{'2'}', then there are no positive integers '{'a,b,c'}' satisfying']
     {display, 'a^n+b^n=c^n.'}
 ]}`
-console.log(stdn.format(example)===example)
-example=``
-console.log(stdn.format(example)===example)
+console.log(stdn.format(example) === example)
+example = ``
+console.log(stdn.format(example) === example)
