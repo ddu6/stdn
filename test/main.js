5e06bfa743c238b314f3f6815cb785d2b22da31d
DDU <ddu6@protonmail.com>
2022-01-24 23:46:01 +0800

    update

--- test/main.js
+++ test/main.js
@@ -4,6 +4,7 @@ let example = `{id t1, mark FLT, desc Fermat, theorem [
     {display, 'a^n+b^n=c^n.'}
 ]}
 ['The first successful proof of '{ref-id t1, ref []}' was released in 1994 by Andrew Wiles.']`
+console.log(stdn.parseWithIndex(example))
 console.log(stdn.format(example) === example)
 example = `{id t1, mark FLT, desc Fermat, theorem [
     ['Let '{'n'}' be an integer greater than '{'2'}', then there are no positive integers '{'a,b,c'}' satisfying']
