c4891616719b2a7d24dee733a56fe9f49dac601e
DDU <ddu6@protonmail.com>
2022-02-14 20:50:53 +0800

    update

--- test/main.js
+++ test/main.js
@@ -1,13 +1,13 @@
 const stdn = require('../mod.c')
-let example = `{id t1, mark FLT, desc Fermat, theorem [
-    ['Let '{'n'}' be an integer greater than '{'2'}', then there are no positive integers '{'a,b,c'}' satisfying']
+let example = `{id flt, mark FLT, desc Fermat, theorem [
+    ['Let '{'n'}' be an integer greater than '{'2,'}' then there are no positive integers '{'a,b,c'}' satisfying']
     {display, 'a^n+b^n=c^n.'}
 ]}
-['The first successful proof of '{ref-id t1, ref []}' was released in 1994 by Andrew Wiles.']`
+['The first successful proof of '{ref-id flt, ref []}' was released in 1994 by Andrew Wiles.']`
 console.log(stdn.parseWithIndex(example))
 console.log(stdn.format(example) === example)
-example = `{id t1, mark FLT, desc Fermat, theorem [
-    ['Let '{'n'}' be an integer greater than '{'2'}', then there are no positive integers '{'a,b,c'}' satisfying']
+example = `{id tlt, mark FLT, desc Fermat, theorem [
+    ['Let '{'n'}' be an integer greater than '{'2,'}' then there are no positive integers '{'a,b,c'}' satisfying']
     {display, 'a^n+b^n=c^n.'}
 ]}`
 console.log(stdn.format(example) === example)
